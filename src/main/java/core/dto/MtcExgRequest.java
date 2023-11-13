34777bc5cf507f6f489e47980eb58740378ec5cd
KIM YOUNGEN <222016531@naver.com>
2023-11-13 20:39:27 +0900

    @@영은 충전DTO 수정

--- src/main/java/core/dto/MtcExgRequest.java
+++ src/main/java/core/dto/MtcExgRequest.java
@@ -7,12 +7,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class MtcExgRequest {
+public class MtcExgRequest extends MtcNcrPayRequest{
 
-    /* 업무구분             */
-    /* 1 : 고객이 충전 요청  */
-    /* 2 : 결제 중 충전 요청 */
-    private Long upmuG;
+    /* 결제여부             */
+    /* Y : 결제 중 충전 요청 */
+    /* N : 고객이 충전 요청  */
+    private String payYn;
 
     /* 계좌번호 (고객번호) */
     private String acno;
@@ -23,8 +23,8 @@ public class MtcExgRequest {
     /* 충전 금액 */
     private Double trxAmt;
 
-    /* 일련번호 */
-    /* 결제 일련번호는 결제에서 충전 요청이 들어온 경우에 필수 사항 */
-    /* 충전 일련번호는 화면 충전 요청들어올 때 채번              */
+    /* 충전 일련번호 : 화면 충전 요청들어올 때 채번 */
     private String acser;
+
+    private MtcNcrPayRequest payRequest;
 }
