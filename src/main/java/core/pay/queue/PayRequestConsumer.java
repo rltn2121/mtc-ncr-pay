e8848b756c0829bb8b3a6c4e30a5621d6312ed59
gayoung <orz4120@gmail.com>
2023-11-23 21:18:13 +0900

    프로젝트명 원복

--- src/main/java/core/pay/queue/PayRequestConsumer.java
+++ src/main/java/core/pay/queue/PayRequestConsumer.java
@@ -22,9 +22,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
 
 @Component
 @RequiredArgsConstructor
@@ -35,78 +32,65 @@ public class PayRequestConsumer {
     private final SdaMainMasRepository sdaMainMasRepository;
     private final MtcPayService mtcPayService;
 
-    @KafkaListener(topics = "mtc.ncr.payRequest", groupId="practice22201653")
+    public String getTimeString()
+    {
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String formatedNow = now.format(formatter);
+
+        LocalTime now2 = LocalTime.now();         // 현재시간 출력
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("HHmmss");
+        String formatedNow2 = now2.format(formatter2);
+
+        return formatedNow+formatedNow2;
+    }
+
+    @KafkaListener(topics = "mtc.ncr.core.payRequest", groupId="practice22201785")
     public void consumeMessage(@Payload MtcNcrPayRequest payReqInfo ,
                                @Header(name = KafkaHeaders.RECEIVED_KEY , required = false) String key ,
                                @Header(KafkaHeaders.RECEIVED_TOPIC ) String topic ,
                                @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp ,
                                @Header(KafkaHeaders.OFFSET) long offset
     ) {
+        log.info("############구독시작한다###############{}" , payReqInfo.toString());
         MtcResultRequest resultDto = new MtcResultRequest();
-        try {
-            log.info("############구독시작한다###############{}", payReqInfo.toString());
-            MtcNcrPayResponse payResponse = new MtcNcrPayResponse();
-
-
-
-            /*
-            SdaMainMas tempAcInfo = sdaMainMasRepository.
-                    findById(new SdaMainMasId(payReqInfo.getAcno(), payReqInfo.getCurC())).orElseThrow();
-            Double ac_jan = tempAcInfo.getAc_jan();
-            log.info("#####ac_jan {} , {}", Double.toString(ac_jan), tempAcInfo.toString());
-            try {
-                if (ac_jan > payReqInfo.getTrxAmt()) // 계좌 잔액이 결제요청금액보다 큰 경우
-                {
-                    resultDto.setAcno(payReqInfo.getAcno());
-                    resultDto.setCurC(payReqInfo.getCurC());
-                    resultDto.setTrxdt(payReqInfo.getTrxDt());
-                    resultDto.setTrxAmt(payReqInfo.getTrxAmt());
-                    resultDto.setAprvSno(payReqInfo.getPayAcser());
-                    //결제처리한다
-                    try {
-                        payResponse = mtcPayService.withdraw(payReqInfo);
-                        //성공했으면 result 큐에 넣어줄 값 셋팅한다.
-                        if (payResponse.getResult() == 1) {
-                            resultDto.setUpmuG(1);
-                            resultDto.setNujkJan(ac_jan - payReqInfo.getTrxAmt());
-                            resultDto.setPayinfo(payReqInfo);
-                            //결과를 result 에 넣는다. ( result 큐에서 거래내역 넣어줌 )
-                            kafkaTemplate.send("mtc.ncr.result", "SUCCESS", resultDto);
-                        } else {
-                            resultDto.setUpmuG(3);
-                            resultDto.setPayinfo(payReqInfo);
-                            resultDto.setNujkJan(ac_jan);
-                            resultDto.setErrMsg(payResponse.getErrStr());
-                            //결과를 result 에 넣는다. ( result 큐에서 거래내역 넣어줌 )
-                            log.info("#####RESULT 큐에 넣어준다. : {}", resultDto.toString());
-                            kafkaTemplate.send("mtc.ncr.result", "FAIL", resultDto);
-                        }
-                    } catch (Exception e) {
-                        log.info("$$$withdraw error : {}", e.toString());
-                        //실패했으면 result 큐에 넣어줄 값 셋팅한다.
-                        resultDto.setUpmuG(3);
-                        resultDto.setNujkJan(ac_jan);
-                        resultDto.setErrMsg(e.toString());
+        MtcNcrPayResponse payResponse = new MtcNcrPayResponse();
+        SdaMainMas tempAcInfo = sdaMainMasRepository.
+                findById(new SdaMainMasId(payReqInfo.getAcno(), payReqInfo.getCurC())).orElseThrow();
+        Double ac_jan = tempAcInfo.getAc_jan();
+        log.info("#####ac_jan {} , {}", Double.toString(ac_jan), tempAcInfo.toString());
+        try
+        {
+            if (ac_jan > payReqInfo.getTrxAmt()) // 계좌 잔액이 결제요청금액보다 큰 경우
+            {
+                resultDto.setAcno(payReqInfo.getAcno());
+                resultDto.setCurC(payReqInfo.getCurC());
+                resultDto.setTrxdt(payReqInfo.getTrxDt());
+                resultDto.setTrxAmt(payReqInfo.getTrxAmt());
+                resultDto.setAprvSno(payReqInfo.getPayAcser());
+                //결제처리한다
+                try{
+                    payResponse = mtcPayService.withdraw(payReqInfo);
+                    //성공했으면 result 큐에 넣어줄 값 셋팅한다.
+                    if(payResponse.getResult()==1)
+                    {
+                        resultDto.setUpmuG(1);
+                        resultDto.setNujkJan(ac_jan- payReqInfo.getTrxAmt());
                         resultDto.setPayinfo(payReqInfo);
+                        //결과를 result 에 넣는다. ( result 큐에서 거래내역 넣어줌 )
+                        kafkaTemplate.send("mtc.ncr.result", "SUCCESS", resultDto);
+                    }
+                    else
+                    {
+                        resultDto.setUpmuG(3);
+                        resultDto.setPayinfo(payReqInfo);
+                        resultDto.setNujkJan(ac_jan);
+                        resultDto.setErrMsg(payResponse.getErrStr());
+                        //결과를 result 에 넣는다. ( result 큐에서 거래내역 넣어줌 )
+                        log.info("#####RESULT 큐에 넣어준다. : {}" , resultDto.toString());
                         kafkaTemplate.send("mtc.ncr.result", "FAIL", resultDto);
                     }
                 }
-                /* 2023.11.23 무한루프로 인해 임시로 막음
-                else //결제요청금액이 잔액보다 큰 경우
-                {
-                    // 충전 큐에 넣는다.
-                    MtcExgRequest exgRequest = new MtcExgRequest();
-                    exgRequest.setPayInfo(payReqInfo);
-                    exgRequest.setAcno(payReqInfo.getAcno());
-                    exgRequest.setCurC(payReqInfo.getCurC());
-                    exgRequest.setAcser(payReqInfo.getPayAcser());
-                    exgRequest.setPayYn("Y");
-                    exgRequest.setTrxAmt(payReqInfo.getTrxAmt() - ac_jan);
-                    kafkaTemplate.send("mtc.ncr.exgRequest", "PAY", exgRequest);
-                    // 업무구분 , 결제 일련번호 , 결제요청금액 , 고객번호
-                }
-
-<<<<<<< HEAD
                 catch(Exception e){
                     log.info("$$$withdraw error : {}" , e.toString());
                     //실패했으면 result 큐에 넣어줄 값 셋팅한다.
@@ -127,30 +111,20 @@ public class PayRequestConsumer {
                 exgRequest.setAcser(payReqInfo.getPayAcser());
                 exgRequest.setPayYn("Y");
                 exgRequest.setTrxAmt(payReqInfo.getTrxAmt()-ac_jan);
-                kafkaTemplate.send("mtc.ncr.exgRequest", "PAY" , exgRequest);
+                // 2023.11.23 무한루프로 인해 임시로 막음kafkaTemplate.send("mtc.ncr.exgRequest", "PAY" , exgRequest);
                 // 업무구분 , 결제 일련번호 , 결제요청금액 , 고객번호
-======= */
             }
-            catch (Exception e) {
-                log.info("$$$$$ 결제하다가 에러난다 : {}", e.toString());
-                log.info("$$$withdraw error : {}", e.toString());
-                //실패했으면 result 큐에 넣어줄 값 셋팅한다.
-                resultDto.setUpmuG(2);
-                resultDto.setNujkJan(ac_jan);
-                resultDto.setErrMsg(e.toString());
-                resultDto.setPayinfo(payReqInfo);
-                kafkaTemplate.send("mtc.ncr.result", "FAIL", resultDto);
-//>>>>>>> refs/remotes/origin/main
-            }
-            */
         }
-        catch (NoSuchElementException e)
+        catch(Exception e)
         {
+            log.info("$$$$$ 결제하다가 에러난다 : {}" , e.toString());
+            log.info("$$$withdraw error : {}" , e.toString());
+            //실패했으면 result 큐에 넣어줄 값 셋팅한다.
             resultDto.setUpmuG(2);
-            resultDto.setNujkJan(0.0);
-            resultDto.setErrMsg("조건에 해당하는 값이 없습니다.");
+            resultDto.setNujkJan(ac_jan);
+            resultDto.setErrMsg(e.toString());
             resultDto.setPayinfo(payReqInfo);
-            kafkaTemplate.send("mtc.ncr.result", "FAIL", resultDto);
+            kafkaTemplate.send("mtc.ncr.result", "FAIL" , resultDto);
         }
     }
 }
